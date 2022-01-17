50f3667a11c4c8c843b6f6d121b8b38221b4fcaa
leowang <rfv7855659@gmail.com>
2022-01-17 18:04:50 +0800

    ✨add unit test & front-end page

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,19 @@
 import '../styles/globals.css'
+import Link from 'next/link'
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+function Market({component,pageProps}){
+  return (
+    <div>
+      <nav className="border-b p=6" style={{backgroundColor: 'purple'}}>
+        <p className="text-4x1 front-bold text-while">Marketplace</p>
+        <Link href="/">
+          <a className="">
+
+          </a>
+        </Link>
+      </nav>
+    </div>
+  )
 }
 
-export default MyApp
+export default Market
--- test/sample-test.js
+++ test/sample-test.js
@@ -1,19 +1,87 @@
 const { expect } = require("chai");
 const { ethers } = require("hardhat");
 
-describe("Greeter", function () {
-  it("Should return the new greeting once it's changed", async function () {
-    const Greeter = await ethers.getContractFactory("Greeter");
-    const greeter = await Greeter.deploy("Hello, world!");
-    await greeter.deployed();
+describe('Market',function(){
+  it('listingPrice should be 0.045 ether',async function(){
+    const Market = await ethers.getContractFactory('Market')
+    const market = await Market.deploy()
+    await market.deployed()
+    
+    let listingPrice = await market.getListingPrice()
+    listingPrice = ethers.utils.formatEther(listingPrice)
+    expect(listingPrice).to.equal('0.045')
+  })
 
-    expect(await greeter.greet()).to.equal("Hello, world!");
+  it('Should makeMarketItem and fetchMarketTokens work',async function() {
+    const Market = await ethers.getContractFactory('Market')
+    const market = await Market.deploy()
+    await market.deployed()
+    const marketAddress = market.address
 
-    const setGreetingTx = await greeter.setGreeting("Hola, mundo!");
+    const NFT = await ethers.getContractFactory('NFT')
+    const nft = await NFT.deploy(marketAddress)
+    await nft.deployed()
+    const nftContractAddress = nft.address
 
-    // wait until the transaction is mined
-    await setGreetingTx.wait();
+     // test for minting
+     await nft.mintToken('https-t1')
+     await nft.mintToken('https-t2')
 
-    expect(await greeter.greet()).to.equal("Hola, mundo!");
-  });
-});
+    let listingPrice = await market.getListingPrice()
+    listingPrice = listingPrice.toString()
+    let auctionPrice = ethers.utils.parseUnits('100','ether')
+    await market.makeMarketItem(nftContractAddress,1,auctionPrice,{value: listingPrice})
+    await market.makeMarketItem(nftContractAddress,2,auctionPrice,{value: listingPrice})
+    let myNFTs = await market.fetchMarketTokens()
+    expect(myNFTs.length).to.equal(2)
+  })
+
+  it('should createMarketSale and fetchMyNFTs work',async function(){
+    const Market = await ethers.getContractFactory('Market')
+    const market = await Market.deploy()
+    await market.deployed()
+    const marketContractAddress = market.address
+
+    const NFT = await ethers.getContractFactory('NFT')
+    const nft = await NFT.deploy(marketContractAddress)
+    await nft.deployed()
+    const nftContractAddress = nft.address
+
+    let listingPrice = await market.getListingPrice()
+    listingPrice = listingPrice.toString()
+    await nft.mintToken('http-1')
+    const auctionPrice = ethers.utils.parseUnits('100','ether')
+    let [addr1] = await ethers.getSigners() 
+    await market.connect(addr1).makeMarketItem(nftContractAddress,1,auctionPrice,{ value:listingPrice })
+
+    await market.connect(addr1).createMarketSale(nftContractAddress,1,{
+      value: auctionPrice
+    })
+
+    const myNFTs = await market.fetchMyNFTs()
+    expect(myNFTs[0].owner).to.equal(addr1.address)
+    expect(myNFTs[0].sold).to.equal(true)
+  })
+
+  it('should fetchItemsCreated work', async function(){
+    const Market = await ethers.getContractFactory('Market')
+    const market = await Market.deploy()
+    await market.deployed()
+    const marketContractAddress = market.address
+
+    const NFT = await ethers.getContractFactory('NFT')
+    const nft = await NFT.deploy(marketContractAddress)
+    await nft.deployed()
+    const nftContractAddress = nft.address
+
+    let listingPrice = await market.getListingPrice()
+    listingPrice = listingPrice.toString()
+    await nft.mintToken('http-1')
+    const auctionPrice = ethers.utils.parseUnits('100','ether')
+    let [addr1] = await ethers.getSigners() 
+    await market.connect(addr1).makeMarketItem(nftContractAddress,1,auctionPrice,{ value:listingPrice })
+
+    let myMints = await market.connect(addr1).fetchItemsCreated()
+    expect(myMints[0].seller).to.equal(addr1.address)
+  })
+})
